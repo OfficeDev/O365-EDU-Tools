230f03b9347700233516d26eceab063b0747dfec
Bill Sluss <wisluss@microsoft.com>
2017-01-03 23:06:38 -0500

    Update Set-Acceptance_Permissions_SDS_Sections.ps1

--- SDS Scripts/Set-Acceptance_Permissions_SDS_Sections.ps1
+++ SDS Scripts/Set-Acceptance_Permissions_SDS_Sections.ps1
@@ -1,7 +1,4 @@
 <#
-Disclaimer:
-This sample script is not supported under any Microsoft standard support program or service. The sample script is provided AS IS without warranty of any kind. Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose. The entire risk arising out of the use or performance of the sample scripts and documentation remains with you. In no event shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages.
-
 Script Name:
 Set-Acceptance_Permissions_SDS_Sections.ps1
 
@@ -20,7 +17,7 @@ Version 1.0, 12/14/2016 - First Draft
 #>
 
 #Import the CSV we exported
-$Users = Import-Csv "C:\temp\Export-Class_Membership_for_Restrictions.csvî
+$Users = Import-Csv "C:\temp\Export-Class_Membership_for_Restrictions.csv‚Äù
 
 #Start Foearch loop against the initial export
 Foreach ($User in $Users) {
