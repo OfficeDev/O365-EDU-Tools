6817d18a0ee13d599d38923cebb256f16b22dff3
Bill Sluss <wisluss@microsoft.com>
2017-01-03 22:56:47 -0500

    Update Get-All_Sections.ps1

--- SDS Scripts/Get-All_Sections.ps1
+++ SDS Scripts/Get-All_Sections.ps1
@@ -1,7 +1,4 @@
 <#
-Disclaimer:
-This sample script is not supported under any Microsoft standard support program or service. The sample script is provided AS IS without warranty of any kind. Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose. The entire risk arising out of the use or performance of the sample scripts and documentation remains with you. In no event shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages.
-
 Script Name:
 Get-All_Sections.ps1
 
@@ -20,7 +17,7 @@ Version 1.0, 12/07/206 - First Draft
 #>
 
 $output = @()
-$Groups = Get-MsolGroup –All | ? {$_.EmailAddress –like “Section_*”}
+$Groups = Get-MsolGroup â€“All | ? {$_.EmailAddress â€“like â€œSection_*â€}
 Foreach ($Group in $Groups) {
 
 	#Display the group being processed
@@ -43,4 +40,4 @@ Foreach ($Group in $Groups) {
 #Export the output array to CSV
 $output | Export-csv c:\temp\Get-All_Sections.csv -NoTypeInformation
 
-Write-host –ForegroundColor Green “Script Complete”
+Write-host â€“ForegroundColor Green â€œScript Completeâ€
