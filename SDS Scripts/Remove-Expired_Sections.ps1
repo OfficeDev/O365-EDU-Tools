1ef25dc5734bb018ff0a3a30bc45108a7ddfed2d
Bill Sluss <wisluss@microsoft.com>
2018-09-04 14:35:48 -0700

    Update Remove-Expired_Sections.ps1

--- SDS Scripts/Remove-Expired_Sections.ps1
+++ SDS Scripts/Remove-Expired_Sections.ps1
@@ -15,16 +15,16 @@ Change Log:
 Version 1.0, 8/9/2018 - First Draft
 #>
 
-$Groups = Get-AzureADGroup -All:$true | ? {$_.DisplayName –like “Exp*”}
+$Groups = Get-AzureADGroup -All:$true | ? {$_.DisplayName -like "Exp*"}
 $Count = $Groups.count
-Write-host –ForegroundColor Green “Found $Count Classes Marked Expired. Starting Cleanup - Remove Sections”
+Write-host -ForegroundColor Green "Found $Count Classes Marked Expired. Starting Cleanup - Remove Sections"
 
 Foreach ($Group in $Groups) {
 $Obj = $Group.objectID
 $DN = $Group.DisplayName
-Write-host –ForegroundColor Green “Removing $DN”
+Write-host -ForegroundColor Green "Removing $DN"
 Remove-AzureADGroup -ObjectID $Obj
  		}
 
 #Export the output array to CSV
-Write-host –ForegroundColor Green “Script Complete”
+Write-host -ForegroundColor Green "Script Complete"
