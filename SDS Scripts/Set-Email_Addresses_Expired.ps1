c38f7416ded3f750536d2ca29e8b9c9b39881e5f
Bill Sluss <wisluss@microsoft.com>
2018-09-24 12:08:08 -0700

    Update and rename Update-EmailAddress.ps1 to Set-Email_Addresses_Expired.ps1

--- SDS Scripts/Set-Email_Addresses_Expired.ps1
+++ SDS Scripts/Set-Email_Addresses_Expired.ps1
@@ -1,13 +1,13 @@
 <#
 
 Script Name:
-Update-EmailAddress.ps1
+Set-Email_Addresses_Expired.ps1
 
 Synopsis:
 This script is designed to get all classes in Exchange Online which have been Marked Expired by SDS. Each of these classes will have their primarySMTPAddress updated with the mailnickname attribute, which is appended with "Expmmyy_". Once the update is complete, PowerShell will generate a report in the c:\temp directory which details every expired class, and the currently set displayname and primary email address.
 
 Syntax Examples and Options:
-.\Update-EmailAddress.ps1 
+.\Set-Email_Addresses_Expired.ps1 
 
 Written By: 
 Bill Sluss
@@ -30,13 +30,13 @@ Foreach ($Group in $ExpGroups) {
 
 #Document the Group Being Processed, incase of errors
 $DN = $Group.DisplayName
-Write-Host -ForegroundColor Green “Updating Email Address for $DN”
+Write-Host -ForegroundColor Green â€œUpdating Email Address for $DNâ€
 
 #Set Variables
 $Alias = $Group.Alias
 $Current = ($Group.primarysmtpaddress).toString()
 $Domain = $Current.Split("@")[1]
-$New = (“$Alias” + “@” + “$Domain”).ToString()
+$New = (â€œ$Aliasâ€ + â€œ@â€ + â€œ$Domainâ€).ToString()
 
 #Update the PrimarySMTPAddress
 Set-UnifiedGroup $Alias -PrimarySMTPAddress $New
@@ -44,6 +44,6 @@ Set-UnifiedGroup $Alias -PrimarySMTPAddress $New
 
 #Export the Expired Group with the updated addresses
 Write-Host " "
-Write-Host -ForegroundColor Red “Generating a report to show all Exp Groups and their new PrimarySMTPAddresses”
+Write-Host -ForegroundColor Red â€œGenerating a report to show all Exp Groups and their new PrimarySMTPAddressesâ€
 $ExpGroups = Get-UnifiedGroup -ResultSize unlimited | ? {$_.alias -like "Exp*"} | select DisplayName, Alias, PrimarySmtpAddress
 $ExpGroups | Export-CSV c:\temp\Expired_Groups.csv -notype
