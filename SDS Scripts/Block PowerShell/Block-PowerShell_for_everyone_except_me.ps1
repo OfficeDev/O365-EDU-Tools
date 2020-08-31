234bb448c8496bcee9c18a68c633f74684936870
Bill Sluss <wisluss@microsoft.com>
2020-08-31 10:44:08 -0400

    Add files via upload

---
+++ SDS Scripts/Block PowerShell/Block-PowerShell_for_everyone_except_me.ps1
@@ -0,0 +1,20 @@
+#Connect to Azure AD and establish a session
+$session = Connect-AzureAD
+
+#set the PowerShell App ID as a variable
+$appId = "1b730954-1685-4b74-9bfd-dac224a7b894"
+
+#Ensure the service principal is present in the tenant, and if not add it
+$sp = Get-AzureADServicePrincipal -Filter "appId eq '$appId'"
+if (-not $sp) {
+    $sp = New-AzureADServicePrincipal -AppId $appId
+}
+
+#Require user assignment for the PowerShell app
+Set-AzureADServicePrincipal -ObjectId $sp.ObjectId -AppRoleAssignmentRequired $true
+
+# Assign the default app role (0-Guid) to the current user
+$me = Get-AzureADUser -ObjectId $session.Account.Id
+New-AzureADServiceAppRoleAssignment -ObjectId $sp.ObjectId -ResourceId $sp.ObjectId -Id ([Guid]::Empty.ToString()) -PrincipalId $me.ObjectId
+
+Write-host "Script Complete. PowerShell is now restricted."
