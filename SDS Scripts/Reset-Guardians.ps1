ebc8a19ba1f89ab4ebfa7949cd55b3275d2569ec
dileeshm <dileeshm@gmail.com>
2021-08-04 16:57:24 +0530

    Refactoring as per the review comments

--- SDS Scripts/Reset-Guardians.ps1
+++ SDS Scripts/Reset-Guardians.ps1
@@ -21,10 +21,10 @@
 .Parameter studentAadObjectIdsCsvFile
     CSV file which has the AAD object Ids of the students for whom the guardians needs to be reset
 .Example
-    Reset guardian for one student: .\Reset-Guardians.ps1 -OutFolder . -clientId "940e3d66-95aa-41d9-237d-45e961889107" -clientSecret "2cJ]-[p19402Ac;Y+7<b>5b" -tenantDomain "contoso.onmicrosoft.com" -studentAadObjectIds "ab043123-00aa-60d9-2ab4-12e961702abc"
-    Reset guardian for multiple students (Student AAD object Ids in a comma separated format): .\Reset-Guardians.ps1 -OutFolder . -clientId "940e3d66-95aa-41d9-237d-45e961889107" -clientSecret "2cJ]-[p19402Ac;Y+7<b>5b" -tenantDomain "contoso.onmicrosoft.com" -studentAadObjectIds "ab043123-00aa-60d9-2ab4-12e961702abc,df043123-00aa-60d9-2ab4-12e961702xyz"
-    Reset guardian for students mentioned in the csv file (Make sure the csv file column header name is studentAadObjectId): .\Reset-Guardians.ps1 -OutFolder . -clientId "940e3d66-95aa-41d9-237d-45e961889107" -clientSecret "2cJ]-[p19402Ac;Y+7<b>5b" -tenantDomain "contoso.onmicrosoft.com" -studentAadObjectIdsCsvFile "studentAadObjectIds.csv"
-    Reset guardian for all SDS synced students : .\Reset-Guardians.ps1 -OutFolder . -clientId "940e3d66-95aa-41d9-237d-45e961889107" -clientSecret "2cJ]-[p19402Ac;Y+7<b>5b" -tenantDomain "contoso.onmicrosoft.com"
+    Reset guardian for one student: .\Reset-Guardians.ps1 -OutFolder . -clientId "743f3d66-95aa-41d9-237d-45e961251889" -clientSecret "8bK]-[p19402Ac;Y+7<b>5b" -tenantDomain "contoso.onmicrosoft.com" -studentAadObjectIds "ab043123-00aa-60d9-2ab4-12e961702abc"
+    Reset guardian for multiple students (Student AAD object Ids in a comma separated format): .\Reset-Guardians.ps1 -OutFolder . -clientId "743f3d66-95aa-41d9-237d-45e961251889" -clientSecret "8bK]-[p19402Ac;Y+7<b>5b" -tenantDomain "contoso.onmicrosoft.com" -studentAadObjectIds "ab043123-00aa-60d9-2ab4-12e961702abc,df043123-00aa-60d9-2ab4-12e961702xyz"
+    Reset guardian for students mentioned in the csv file (Make sure the csv file column header name is studentAadObjectId): .\Reset-Guardians.ps1 -OutFolder . -clientId "743f3d66-95aa-41d9-237d-45e961251889" -clientSecret "8bK]-[p19402Ac;Y+7<b>5b" -tenantDomain "contoso.onmicrosoft.com" -studentAadObjectIdsCsvFile "studentAadObjectIds.csv"
+    Reset guardian for all SDS synced students : .\Reset-Guardians.ps1 -OutFolder . -clientId "743f3d66-95aa-41d9-237d-45e961251889" -clientSecret "8bK]-[p19402Ac;Y+7<b>5b" -tenantDomain "contoso.onmicrosoft.com"
 #>
 
 Param (
@@ -49,13 +49,14 @@ Param (
 
 $logFilePath = "$OutFolder\ResetGuardians.log"
 
-$global:lastRefreshed = $null
-function Refresh-AccessToken() {    
+function Refresh-AccessToken($authToken, $lastRefreshed) {    
     $dateNow = get-date
-    if ($global:lastRefreshed -eq $null -or ($dateNow - $global:lastRefreshed).Minutes -gt 55) {
+    if ($lastRefreshed -eq $null -or ($dateNow - $lastRefreshed).Minutes -gt 55) {
         Write-Host "Refreshing Access token"
-        Get-AccessToken        
-    }  
+        $authToken, $lastRefreshed = Get-AccessToken       
+    } 
+
+    return $authToken, $lastRefreshed
 }
 
 function Get-AccessToken() {
@@ -70,41 +71,42 @@ function Get-AccessToken() {
 
     Write-Host "Getting access token"
     $tokenResponse = Invoke-RestMethod -Method POST -Uri $tokenUrl -Body $tokenBody
-    $global:authToken = $tokenResponse.access_token
-    $global:lastRefreshed = get-date    
+    $authToken = $tokenResponse.access_token
+    $lastRefreshed = get-date    
     } catch {
         Write-Error -Exception $_ -Message "Failed to get authentication token for Microsoft Graph. Please check the client Id and secret provided."
-        $global:authToken = $null
+        $authToken = $null
     }
+
+    return $authToken, $lastRefreshed
 }
 
-function Reset-GuardiansForUser($userId) {
-    Refresh-AccessToken
+function Reset-GuardiansForUser($userId, $authToken, $lastRefreshed) {
+    $authToken, $lastRefreshed = Refresh-AccessToken -authToken $authToken -lastRefreshed $lastRefreshed
     $noContacts = @{
         relatedContacts = @()
     }
     Write-Progress -Activity "Resetting guardians for user $userId"    
     $noContactsBody = ConvertTo-Json $noContacts    
-    Invoke-RestMethod -Method Patch -Uri "https://graph.microsoft.com/beta/education/users/$userId" -Body $noContactsBody -Headers @{'Authorization'="Bearer $global:authToken"} -ContentType "application/json" | out-file $logFilePath -Append
+    Invoke-RestMethod -Method Patch -Uri "https://graph.microsoft.com/beta/education/users/$userId" -Body $noContactsBody -Headers @{'Authorization'="Bearer $authToken"} -ContentType "application/json" | out-file $logFilePath -Append
 }
 
-function Get-AllStudents() {
+function Get-AllStudents($authToken, $lastRefreshed) {
     Write-Host "Getting All SDS synced students"
     $students = @()
     $nextLink = "https://graph.microsoft.com/beta/users/?`$filter=extension_fe2174665583431c953114ff7268b7b3_Education_ObjectType eq 'Student'&`$select=id,displayName"
-    while ($nextLink -ne $null) {
-        Write-Progress -Activity "Getting All SDS synced students"        
-        $response = Invoke-RestMethod -Method Get -Uri $nextLink -Headers @{'Authorization'="Bearer $global:authToken"}
+    while ($nextLink -ne $null) {               
+        $response = Invoke-RestMethod -Method Get -Uri $nextLink -Headers @{'Authorization'="Bearer $authToken"}
         $students += $response.value
         $nextLink = $response.'@odata.nextLink'
-        Refresh-AccessToken        
+        $authToken, $lastRefreshed = Refresh-AccessToken -authToken $authToken -lastRefreshed $lastRefreshed        
     }
     return $students
 }
 
-Get-AccessToken
+$authToken, $lastRefreshed = Get-AccessToken
 
-if ($global:authToken -eq $null) {
+if ($authToken -eq $null) {
     Write-Host "Authentication Failed"
     return
 }
@@ -113,26 +115,21 @@ if ($studentAadObjectIds -ne "") {
     $studentAadObjectIdsArr = $studentAadObjectIds.split(',')
     foreach($studentAadObjectId in $studentAadObjectIdsArr) {
         Write-Host "Resetting guardians for student object Id $studentAadObjectId"
-        Reset-GuardiansForUser -userId $studentAadObjectId
+        Reset-GuardiansForUser -userId $studentAadObjectId -authToken $authToken -lastRefreshed $lastRefreshed
     }    
 } elseif ($studentAadObjectIdsCsvFile -ne "") {
-    if ($global:authToken -eq $null) {
-        Write-Host "Authentication Failed"
-        return
-    }
-
     $studentAadObjectIdList = import-csv $studentAadObjectIdsCsvFile
     foreach($studentAadObject in $studentAadObjectIdList) {
         Write-Host "Resetting guardians for student object Id $($studentAadObject.studentAadObjectId)"
-        Reset-GuardiansForUser -userId $studentAadObject.studentAadObjectId
+        Reset-GuardiansForUser -userId $studentAadObject.studentAadObjectId -authToken $authToken -lastRefreshed $lastRefreshed
     }    
 } else {
-    $students = Get-AllStudents
+    $students = Get-AllStudents -authToken $authToken -lastRefreshed $lastRefreshed
     $reply = Read-Host -Prompt "Will reset guardians for $($students.length) students. Continue? [y/n]"
     if ($reply -match "[yY]") {
         foreach($student in $students) {
             Write-Host "Resetting guardians for $($student.displayName), id $($student.id)"
-            Reset-GuardiansForUser -userId $student.id
+            Reset-GuardiansForUser -userId $student.id -authToken $authToken -lastRefreshed $lastRefreshed
         }    
     }
 }
