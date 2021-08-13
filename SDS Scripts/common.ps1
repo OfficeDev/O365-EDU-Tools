72129c269fb60c087287a0fa2f1bd1903b73e16c
AyronJohnson <ayjohnso@microsoft.com>
2021-08-13 16:42:33 -0400

    Update Rename and Unrename expired classes to use Graph API and common.ps1 token refresh logic

--- SDS Scripts/common.ps1
+++ SDS Scripts/common.ps1
@@ -14,12 +14,14 @@ Version 1.0, 07/26/2021 - First Draft
 
 function Initialize($graphscopes) {
     import-module Microsoft.Graph.Authentication -MinimumVersion 0.9.1
-    Write-Output "If prompted, please use a tenant admin-account to grant access to $graphscopes privileges"
-    Refresh-Token $null $graphscopes
+    $null = Write-Output "If prompted, please use a tenant admin-account to grant access to $graphscopes privileges"
+    $firstToken = Refresh-Token $null $graphscopes
+    return $firstToken
 }
 function Refresh-Token($lastRefreshed, $graphscopes) {
-    if ($lastRefreshed -eq $null -or (get-date - $lastRefreshed).Minutes -gt 10) {
-        connect-graph -scopes $graphscopes
+    $currentDT = get-date
+    if ($lastRefreshed -eq $null -or (New-TimeSpan -Start $currentDT -End $lastRefreshed).Minutes -gt 10) {
+        connect-graph -scopes $graphscopes | Out-Null
         $lastRefreshed = get-date
     }
     return $lastRefreshed
@@ -33,7 +35,7 @@ function PageAll-GraphRequest($initialUri, $refreshToken, $method, $graphscopes,
     $currentUrl = $initialUri
     
     while ($currentUrl -ne $null) {
-        Refresh-Token $lastRefreshed $graphscopes
+        Refresh-Token $refreshToken $graphscopes
         $response = invoke-graphrequest -Method $method -Uri $currentUrl -ContentType "application/json"
         $result += $response.value
         $currentUrl = $response.'@odata.nextLink'
