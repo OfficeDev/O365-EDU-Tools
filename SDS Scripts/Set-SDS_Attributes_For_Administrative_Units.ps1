9d6162c63c02501b83ef2bc3dcb71bda8f810aca
AyronJohnson <14080821+AyronJohnson@users.noreply.github.com>
2021-12-15 16:21:46 -0500

    Update Set-SDS_Attributes_For_Administrative_Units.ps1
    added scopes

--- SDS Scripts/Set-SDS_Attributes_For_Administrative_Units.ps1
+++ SDS Scripts/Set-SDS_Attributes_For_Administrative_Units.ps1
@@ -73,7 +73,7 @@ function Get-AdministrativeUnits {
         # Check if need to renew connection
         $currentDT = Get-Date
         if ($lastRefreshed -eq $null -or (New-TimeSpan -Start $currentDT -End $lastRefreshed).Minutes -gt 10) {
-            Connect-Graph | Out-Null
+            Connect-Graph -scope "AdministrativeUnit.Read.All, Directory.Read.All, AdministrativeUnit.ReadWrite.All, Directory.ReadWrite.All, Directory.AccessAsUser.All" | Out-Null
             $lastRefreshed = Get-Date
         }
 
@@ -177,7 +177,7 @@ if ((Test-Path $outFolder) -eq 0)
 Write-Host "`nActivity logged to file $logFilePath `n" -ForegroundColor Green
 
 # Connecting to resources
-Connect-Graph
+Connect-Graph -scope "AdministrativeUnit.Read.All, Directory.Read.All, AdministrativeUnit.ReadWrite.All, Directory.ReadWrite.All, Directory.AccessAsUser.All"
 
 Get-AdministrativeUnits
 Set-SDS_Attributes_For_AUs
