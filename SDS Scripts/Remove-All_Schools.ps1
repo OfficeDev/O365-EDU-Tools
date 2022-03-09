386c08494144271fbff7d05c1802598966ef1d69
AyronJohnson <ayjohnso@microsoft.com>
2022-03-09 17:37:55 -0500

    Replaced msol and adal cmdlets and logic with graph

--- SDS Scripts/Remove-All_Schools.ps1
+++ SDS Scripts/Remove-All_Schools.ps1
@@ -1,39 +1,24 @@
 <#
-Script Name:
-Remove-All_Schools.ps1
-
-Synopsis:
+.SYNOPSIS
 This script is designed to remove all Schools created by SDS from an O365 tenant. You will need to enter your credentials 2 times as the script sets up the connection to Azure, and then confirm you want to run the script with a "y". Once the script completes, a folder called "true" will be created in the same directory as the script itself, and contain an output file which details the schools removed.
 
-Syntax Examples and Options:
+.EXAMPLE
 .\Remove-All_Schools.ps1 -RemoveSchoolAUs $true
-
-Written By: 
-SDS Team, and adapted by Bill Sluss
-
-Change Log:
-Version 1, 12/12/2016 - First Draft
 
 #>
 
 Param (
-    [switch] $RemoveSchoolAUs = $false,
-    #[switch] $RemoveSectionGroupMemberships = $false,
-    #[switch] $RemoveSectionGroups = $false,
-    #[switch] $RemoveSchoolAUMemberships = $false,
-    [string] $OutFolder = ".",
+    [Parameter(Mandatory=$false)]
+    [string] $outFolder = ".\Remove-All_Schools",
+    [Parameter(Mandatory=$false)]
+    [string] $graphVersion = "beta",
+    [Parameter(Mandatory=$false)]
+    [string] $skipToken= ".",
     [switch] $PPE = $false
 )
 
-$RemoveSectionGroups = $false
-
-$GraphEndpointProd = "https://graph.windows.net"
-$AuthEndpointProd = "https://login.windows.net"
-
-$GraphEndpointPPE = "https://graph.ppe.windows.net"
-$AuthEndpointPPE = "https://login.windows-ppe.net"
-
-$NugetClientLatest = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+$graphEndpointProd = "https://graph.windows.com"
+$graphEndpointPPE = "https://graph.ppe.windows.com"
 
 function Get-PrerequisiteHelp
 {
@@ -42,131 +27,26 @@ function Get-PrerequisiteHelp
  Required Prerequisites
 ========================
 
-1. Install Microsoft Online Services Sign-In Assistant v7.0 from http://www.microsoft.com/en-us/download/details.aspx?id=39267
+1. Install Microsoft Graph Powershell Module with the command 'Install-Module Microsoft.Graph'
 
-2. Install the AAD PowerShell Module from http://msdn.microsoft.com/en-us/library/azure/jj151815.aspx#bkmk_installmodule
-
-3. Check that you can connect to your tenant directory from the PowerShell module to make sure everything is set up correctly.
+2. Check that you can connect to your tenant directory from the PowerShell module to make sure everything is set up correctly.
 
     a. Open a separate PowerShell session
     
-    b. Execute: "Connect-MsolService" to bring up a sign in UI 
+    b. Execute: "Connect-Graph" to bring up a sign-in UI. 
     
-    c. Sign in with any tenant administrator credentials
+    c. Sign in with any tenant administrator credentials.
     
-    d. If you are returned to the PowerShell sesion without error, you are correctly set up
+    d. If you are returned to the PowerShell session without error, you are correctly set up.
 
-5. Retry this script.  If you still get an error about failing to load the MSOnline module, troubleshoot why "Import-Module MSOnline" isn't working
+3.  Ensure that you have application setup correctly in Azure Active Directory with the following permission scopes: AdministrativeUnit.Read.All, Directory.Read.All, AdministrativeUnit.ReadWrite.All, Directory.ReadWrite.All, Directory.AccessAsUser.All
+
+4.  Retry this script.  If you still get an error about failing to load the Microsoft Graph module, troubleshoot why "Import-Module Microsoft.Graph.Authentication" isn't working.
 
 (END)
 ========================
 "@
 }
-
-function Load-ActiveDirectoryAuthenticationLibrary 
-{
-	$moduleDirPath = ($ENV:PSModulePath -split ';')[0]
-	$modulePath = $moduleDirPath + "\AADGraph"
-	if(-not (Test-Path ($modulePath+"\Nugets"))) {New-Item -Path ($modulePath+"\Nugets") -ItemType "Directory" | out-null}
-	$adalPackageDirectories = (Get-ChildItem -Path ($modulePath+"\Nugets") -Filter "Microsoft.IdentityModel.Clients.ActiveDirectory*" -Directory)
-	if($adalPackageDirectories.Length -eq 0){
-        # Get latest nuget client
-        $nugetClientPath = $modulePath + "\Nugets\nuget.exe"
-        Remove-Item -Path $nugetClientPath -Force -ErrorAction Ignore
-		Write-Verbose "Downloading latest nuget client from $NugetClientLatest"
-		$wc = New-Object System.Net.WebClient
-		$wc.DownloadFile($NugetClientLatest, $nugetClientPath);
-		
-        # Install ADAL nuget package
-		$nugetDownloadExpression = $nugetClientPath + " install Microsoft.IdentityModel.Clients.ActiveDirectory -source https://www.nuget.org/api/v2/ -Version 2.19.208020213 -OutputDirectory " + $modulePath + "\Nugets"
-        Write-Verbose "Active Directory Authentication Library Nuget doesn't exist. Downloading now: `n$nugetDownloadExpression"
-		Invoke-Expression $nugetDownloadExpression
-	}
-
-	$adalPackageDirectories = (Get-ChildItem -Path ($modulePath+"\Nugets") -Filter "Microsoft.IdentityModel.Clients.ActiveDirectory*" -Directory)
-    if ($adalPackageDirectories -eq $null -or $adalPackageDirectories.length -le 0)
-    {
-        Write-Error "Unable to download ADAL nuget package"
-        return $false
-    }
-
-    $adal4_5Directory = Join-Path $adalPackageDirectories[$adalPackageDirectories.length-1].FullName -ChildPath "lib\net45"
-	$ADAL_Assembly = Join-Path $adal4_5Directory -ChildPath "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
-	$ADAL_WindowsForms_Assembly = Join-Path $adal4_5Directory -ChildPath "Microsoft.IdentityModel.Clients.ActiveDirectory.WindowsForms.dll"
-
-	if($ADAL_Assembly.Length -gt 0 -and $ADAL_WindowsForms_Assembly.Length -gt 0){
-		Write-Verbose "Loading ADAL Assemblies: `n`t$ADAL_Assembly `n`t$ADAL_WindowsForms_Assembly"
-        Write-Debug "file path length for $ADAL_Assembly is $($ADAL_Assembly.Length)"
-		[System.Reflection.Assembly]::LoadFrom($ADAL_Assembly) | out-null
-		[System.Reflection.Assembly]::LoadFrom($ADAL_WindowsForms_Assembly) | out-null
-		return $true
-	}
-	else{
-		Write-Verbose "Fixing Active Directory Authentication Library package directories ..."
-		$adalPackageDirectories | Remove-Item -Recurse -Force | Out-Null
-		Write-Error "Not able to load ADAL assembly. Delete the Nugets folder under" $modulePath ", restart PowerShell session and try again ..."
-	}
-
-    return $false
-}
-
-<#
-.Synopsis
-    Get authentication result. This is to acquire an OAuth2token for graph API calls.
-#>
-function Get-AuthenticationResult()
-{
-  $clientId = "1950a258-227b-4e31-a9cf-717495945fc2"
-  $redirectUri = [Uri] "urn:ietf:wg:oauth:2.0:oob"
-  $resourceClientId = "00000002-0000-0000-c000-000000000000"
-  $resourceAppIdURI = $graphEndPoint
-  $authority = $authEndPoint + "/common"
- 
-  $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority,$false
-  $promptBehavior = [Microsoft.IdentityModel.Clients.ActiveDirectory.PromptBehavior]::Always
-  $platformParameter = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters" -ArgumentList $promptBehavior
-  $authResult = $authContext.AcquireTokenAsync([string] $resourceAppIdURI, [string] $clientId, [Uri] $redirectUri, $platformParameter).Result
-  
-  Write-Output $authResult
-}
-
-function Send-WebRequest
-{
-    Param
-    (
-        $method,
-        $uri,
-        $payload
-    )
-
-    $response = ""
-    $tokenExpiredRetryCount = 0
-    Do {
-        if ($tokenExpiredRetryCount -gt 0) {
-            $authToken = Get-AuthenticationResult
-        }
-
-        if ($method -ieq "get") {
-            $headers = @{ "Authorization" = "Bearer " + $authToken.AccessToken }
-			Write-Output $uri
-            $response = Invoke-WebRequest -Method $method -Uri $uri -Headers $headers
-        }
-        else {
-            $headers = @{ 
-                "Authorization" = "Bearer " + $authToken.AccessToken
-                "Accept" = "application/json;odata=minimalmetadata"
-                "Content-Type" = "application/json"
-            }
-
-            $response = Invoke-WebRequest -Method $method -Uri $uri -Headers $headers -Body $payload
-        }
-
-        $tokenExpiredRetryCount++
-    } While (($response -contains "Authentication_ExpiredToken") -and  ($tokenExpiredRetryCount -lt 5))
-
-    Write-Output $response
-}
-
 
 function Get-AdministrativeUnits
 {
@@ -175,34 +55,60 @@ function Get-AdministrativeUnits
         $eduObjectType
     )
 
-    $fileName = $eduObjectType + "-AUs-" + $authToken.TenantId +".csv"
-	$filePath = Join-Path $OutFolder $fileName
-    Remove-Item -Path $filePath -Force -ErrorAction Ignore
+    $fileName = $eduObjectType + "-AUs-" + $tenantId +".csv"
+	$csvfilePath = Join-Path $outFolder $fileName
+    Remove-Item -Path $csvfilePath -Force -ErrorAction Ignore
     
-    $firstPage = $true
-    Do
+    # Removes csv file unless link is provided to resume
+    if ((Test-Path $csvFilePath) -and ($skipToken -eq "."))
     {
-        if ($firstPage)
-        {
-            $uri = $graphEndPoint + "/" + $authToken.TenantId + "/administrativeUnits?api-version=beta&`$filter=extension_fe2174665583431c953114ff7268b7b3_Education_ObjectType%20eq%20'$eduObjectType'"
-            "ObjectId, DisplayName" | Out-File $filePath -Append
-            $firstPage = $false
-        }
-        else
-        {
-            $uri = $graphEndPoint + "/" + $authToken.TenantId + "/" + $responseObject.odatanextLink + "&api-version=beta"
-        }
-        # Write-Host "GET: $uri"
-
-        $response = Send-WebRequest "Get" $uri
-        $responseString = $response.Content.Replace("odata.", "odata")
-        $responseObject = $responseString | ConvertFrom-Json
-        foreach ($au in $responseObject.value)
-        {
-            $au.ObjectId + ", " + $au.DisplayName | Out-File $filePath -Append
-        }
+ 	    Remove-Item $csvFilePath;
     }
-    While ($responseObject.odatanextLink -ne $null)
+
+    $auList = @() # Array of objects for AUs
+    $pageCnt = 1 # Counts the number of pages of AUs retrieved
+    $lastRefreshed = $null # Used for refreshing connection
+
+    # Get all AU's of Edu Object Type School
+    Write-Progress -Activity "Reading AAD" -Status "Fetching School Administrative Units"
+
+    do {
+        # Check if need to renew connection
+        $currentDT = Get-Date
+        if ($lastRefreshed -eq $null -or (New-TimeSpan -Start $currentDT -End $lastRefreshed).Minutes -gt 10) {
+            Connect-Graph -scopes "AdministrativeUnit.Read.All, Directory.Read.All, AdministrativeUnit.ReadWrite.All, Directory.ReadWrite.All, Directory.AccessAsUser.All" | Out-Null
+            $lastRefreshed = Get-Date
+        }
+
+        $auUri = "$graphEndPoint/$graphVersion/directory/administrativeUnits?`$select=id,displayName,extension_fe2174665583431c953114ff7268b7b3_Education_ObjectType"
+
+        if ($skipToken -ne "." ) {
+            $auUri = $skipToken
+        }
+
+        $auResponse = 
+         -Uri $auUri -Method GET
+        $aus = $auResponse.value
+        
+        $auCtr = 1 # Counter for AUs retrieved
+        
+        foreach ($au in $aus){
+            if ( ($au.extension_fe2174665583431c953114ff7268b7b3_Education_ObjectType -eq $eduObjectType) ) { # Filtering out AU already with SDS attribute
+                $obj = [pscustomobject]@{"ObjectId"=$au.Id;"DisplayName"=$au.DisplayName;}
+                $auList += $obj
+                $auCtr++
+            }
+        }
+
+        Write-Progress -Activity "Retrieving School Administrative Units..." -Status "Retrieved $auCtr AUs from $pageCnt pages"
+        
+        # Write nextLink to log if need to restart from previous page
+        Write-Output "[$(Get-Date -Format G)] Retrieved $pageCnt pages. nextLink: $($graphResponse.'@odata.nextLink')" | Out-File $logFilePath -Append
+        $pageCnt++
+
+    } while ($auResponse.'@odata.nextLink')  
+
+    $auList | Export-Csv $csvFilePath -Append -NotypeInformation
 
     return $filePath
 }
@@ -216,17 +122,20 @@ function Remove-AdministrativeUnits
 
     Write-Host "WARNING: You are about to remove Administrative Units and its memberships created from SDS. `nIf you want to skip removing any AUs, edit the file now and remove the corresponding lines before proceeding. `n" -ForegroundColor Yellow
     Write-Host "Proceed with deleting all the AUs logged in $auListFileName (yes/no)?" -ForegroundColor White
+    
     $choice = Read-Host
+
     if ($choice -ieq "y" -or $choice -ieq "yes")
     {
         Write-Progress -Activity $activityName -Status "Deleting Administrative Units"
         $auList = import-csv $auListFileName
         $auCount = $auList.Length
         $index = 1
+
         Foreach ($au in $auList) 
         {
             Write-Output "[$index/$auCount] Removing AU `"$($au.DisplayName)`" [$($au.ObjectId)] from directory"
-            Remove-MsolAdministrativeUnit -ObjectId $au.ObjectId -Force
+            Remove-MgAdministrativeUnit -AdministrativeUnitId $au.ObjectId -Confirm:$true
             $index++
         }
     }
@@ -234,63 +143,51 @@ function Remove-AdministrativeUnits
 
 
 # Main
+$graphEndPoint = $graphEndpointProd
+$graphScopes = "AdministrativeUnit.Read.All, Directory.Read.All, AdministrativeUnit.ReadWrite.All, Directory.ReadWrite.All, Directory.AccessAsUser.All"
+$logFilePath = "$outFolder\Remove-All_Schools.log"
+
 $graphEndPoint = $GraphEndpointProd
-$authEndPoint = $AuthEndpointProd
+
 if ($PPE)
 {
     $graphEndPoint = $GraphEndpointPPE
-    $authEndPoint = $AuthEndpointPPE
 }
-
-$activityName = "Cleaning up SDS Objects in Directory"
 
 try
 {
-    Import-Module MSOnline | Out-Null
+    Import-Module Microsoft.Graph.Authentication | Out-Null
 }
 catch
 {
-    Write-Error "Failed to load MSOnline PowerShell Module."
+    Write-Error "Failed to load Microsoft Graph PowerShell Module."
     Get-PrerequisiteHelp | Out-String | Write-Error
     throw
 }
 
-# Connect to the tenant
-Write-Progress -Activity $activityName -Status "Connecting to tenant"
-Connect-MsolService -ErrorAction Stop
+# Create output folder if it does not exist
+if ((Test-Path $outFolder) -eq 0)
+{
+	mkdir $outFolder;
+}
 
-$adalLoaded = Load-ActiveDirectoryAuthenticationLibrary
-if ($adalLoaded)
-{
-    $authToken = Get-AuthenticationResult
-}
-else
-{
-    Write-Error "Could not load dependent libraries required by the script."
-    Get-PrerequisiteHelp | Out-String | Write-Error
-    Exit
-}
+# Connecting to resources
+Connect-Graph -scopes $graphScopes
+
+$tenantInfo = Get-MgOrganization
+$tenantId =  $tenantInfo.Id
+
+$activityName = "Cleaning up SDS Objects in Directory"
 
 Write-Progress -Activity $activityName -Status "Connected. Discovering tenant information"
-$tenantInfo = Get-MsolCompanyInformation
-$tenantId =  $tenantInfo.ObjectId
-$tenantDisplayName = $tenantInfo.DisplayName
+Write-Host "`nActivity logged to file $logFilePath `n" -ForegroundColor Green
 
-# Create output folder if it does not exist
-if ((Test-Path $OutFolder) -eq 0)
-{
-	mkdir $OutFolder;
-}
+# Get all AUs of Edu Object Type School
+Write-Progress -Activity $activityName -Status "Fetching School Administrative Units"
+$outputFileName = Get-AdministrativeUnits "School"
+Write-Host "`nSchool Administrative Units logged to file $outputFileName `n" -ForegroundColor Green
 
-if ($RemoveSchoolAUs -eq $true)
-{
-    # Get all AUs of Edu Object Type School
-    Write-Progress -Activity $activityName -Status "Fetching School Administrative Units"
-    $OutputFileName = Get-AdministrativeUnits "School"
-    Write-Host "`nSchool Administrative Units logged to file $OutputFileName `n" -ForegroundColor Green
+# Delete School AUs
+Remove-AdministrativeUnits $outputFileName
 
-    # Delete School AUs
-    Remove-AdministrativeUnits $OutputFileName
-}
-
-Write-Output "`nDone.`n"
+Write-Output "`n`nDone.  Please run 'Disconnect-Graph' if you are finished.`n"
