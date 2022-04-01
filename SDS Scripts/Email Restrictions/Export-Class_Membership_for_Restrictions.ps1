b5266cafbcbf5dd2dee923184837598c99a1f6c0
SarathSrestaluri <v-sresar@microsoft.com>
2022-04-01 16:16:31 +0530

    Export

--- SDS Scripts/Email Restrictions/Export-Class_Membership_for_Restrictions.ps1
+++ SDS Scripts/Email Restrictions/Export-Class_Membership_for_Restrictions.ps1
@@ -1,58 +1,27 @@
 ﻿<#
-Script Name:
-Export-Class_Membership_for_Restrictions.ps1
-
-Written By: 
-Bill Sluss and adapted by Srestaluri Sarath
-
-Change Log: 
-Version 1.0, 12/12/2016 - First Draft
-Version 2.0, 03/22/2022 - Updated script to use Graph API instead of ADAL
-#>
-
-<#
 .SYNOPSIS
     Export-Class_Membership_for_Restrictions.ps1 is designed to export each section/group synced through SDS, and provide a user to class mapping for every user and class in the organization. This output will be used to stamp the acceptance permissions on each of the respective mailboxes.
-.EXAMPLE    
+
+.Description
+    The script will interact with Microsoft online resources using the Graph module. Once connected, the script will get the GroupMembers. A folder will be created in the same directory as the script itself containing log file and a csv file with the data previously mentioned.
+
+.PARAMETER outFolder
+    Path where to put the log and csv file with the fetched users.
+
+.PARAMETER graphVersion
+    The version of the Graph API.
+
+.EXAMPLE
     .\Export-Class_Membership_for_Restrictions.ps1
-#>
 
-Param(
-    [switch] $PPE,
-    [Parameter(Mandatory = $false)]
-    [string] $OutFolder = ".",
-    # Parameter to specify whether to download the script with common functions or not
-    [switch] $skipDownloadCommonFunctions
-)
+.NOTES
+***This script may take a while.***
 
-$GraphEndpointProd = "https://graph.microsoft.com"
-$GraphEndpointPPE = "https://graph.microsoft-ppe.com"
-
-$logFilePath = "$OutFolder\SDSGroups.log"
-
-if ($skipDownloadCommonFunctions -eq $false) {
-    # Downloading file with latest common functions
-    try {
-        Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OfficeDev/O365-EDU-Tools/master/SDS%20Scripts/common.ps1" -OutFile ".\common.ps1" -ErrorAction Stop -Verbose
-        "Grabbed 'common.ps1' to the directory alongside the executing script"
-        Write-Output "[$(get-date -Format G)] Grabbed 'common.ps1' to the directory alongside the executing script. common.ps1 script contains common functions, which can be used by other SDS scripts" | out-file $logFilePath -Append
-
-    } 
-    catch {
-        throw "Unable to download common.ps1"
-    }
-}
-
-#import file with common functions
-. .\common.ps1
-
-function Get-PrerequisiteHelp {
-    Write-Output @"
 ========================
- Required Prerequisites
+Required Prerequisites
 ========================
 
-1. Install Microsoft Graph Powershell Module with command 'Install-Module Microsoft.Graph'. 
+1. Install Microsoft Graph Powershell Module with command 'Install-Module Microsoft.Graph'.
 PowerShell 7 and later is the recommended PowerShell version for use with the Microsoft Graph PowerShell SDK on all platforms (https://docs.microsoft.com/en-us/graph/powershell/installation#supported-powershell-versions).
 
 2. Make sure to download common.ps1 to the same folder of the script which has common functions needed.  https://github.com/OfficeDev/O365-EDU-Tools/blob/master/SDS%20Scripts/common.ps1
@@ -62,22 +31,54 @@ Command to download the function: Invoke-WebRequest -Uri "https://raw.githubuser
 
     a. Open a separate PowerShell session
     
-    b. Execute: "connect-graph -scopes GroupMember.ReadWrite.All, Group.ReadWrite.All, Directory.ReadWrite.All, Directory.AccessAsUser.All" to bring up a sign in UI. 
+    b. Execute: "connect-graph -scopes GroupMember.ReadWrite.All, Group.ReadWrite.All, Directory.ReadWrite.All, Directory.AccessAsUser.All" to bring up a sign in UI.
     
     c. Sign in with any tenant administrator credentials
     
-    d. If you are returned to the PowerShell sesion without error, you are correctly set up
+    d. If you are returned to the PowerShell session without error, you are correctly set up
 
 4. Retry this script.  If you still get an error about failing to load the Microsoft Graph module, troubleshoot why "Import-Module Microsoft.Graph.Authentication -MinimumVersion 0.9.1" isn't working
+#>
 
-(END)
-========================
-"@
+Param(
+    [switch] $PPE = $false,
+    [Parameter(Mandatory=$false)]
+    [string] $outFolder = ".\SDS_Class_Membership",
+    [Parameter(Mandatory=$false)]
+    [string] $graphVersion = "beta",
+    # Parameter to specify whether to download the script with common functions or not
+    [switch] $skipDownloadCommonFunctions
+)
+
+$GraphEndpointProd = "https://graph.microsoft.com"
+$GraphEndpointPPE = "https://graph.microsoft-ppe.com"
+
+$logFilePath = "$outFolder\Export-Class_Membership_for_Restrictions.log"
+
+# Create output folder if it does not exist
+if ((Test-Path $outFolder) -eq 0) {
+    mkdir $outFolder | Out-Null;
 }
+
+if ($skipDownloadCommonFunctions -eq $false) {
+    # Downloading file with latest common functions
+    try {
+        Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OfficeDev/O365-EDU-Tools/master/SDS%20Scripts/common.ps1" -OutFile ".\common.ps1" -ErrorAction Stop -Verbose
+        "Grabbed 'common.ps1' to the directory alongside the executing script"
+        Write-Output "[$(get-date -Format G)] Grabbed 'common.ps1' to the directory alongside the executing script. common.ps1 script contains common functions, which can be used by other SDS scripts" | out-file $logFilePath -Append
+
+    }
+    catch {
+        throw "Unable to download common.ps1"
+    }
+}
+
+#import file with common functions
+. .\common.ps1
 
 function Get-Groups($graphEndPoint, $refreshToken, $method, $graphScopes, $logFilePath) {
 
-    $groupsUri = "$graphEndPoint/beta/groups"
+    $groupsUri = "$graphEndPoint/$graphVersion/groups"
 
     $groups = PageAll-GraphRequest $groupsUri $refreshToken $method $graphScopes $logFilePath | Where-Object  {$_.mail -like "*Section_*"}
     
@@ -87,25 +88,30 @@ function Get-Groups($graphEndPoint, $refreshToken, $method, $graphScopes, $logFi
 function Get-GroupMembers($groupList, $graphEndPoint, $refreshToken, $method, $graphScopes, $logFilePath){
     #Build the Array
     $Results = @()
+    $index = 1
     
     foreach ($group in $groupList) {
-        $DN = $group.displayName
-        $GroupObjectID = $group.Id
-        $Addr = $group.mail
-        Write-Output "Processing Memberships for Group `"$($group.displayName)`" [$($group.Id)] from directory" | out-file $logFilePath -Append  
-        Write-Host -ForegroundColor Green "Exporting data for group $DN"
-        $groupMembersUri = $graphEndPoint + '/beta/groups/' + $group.Id + '/members'
+        $name = $group.displayName
+        $id = $group.Id
+        $addr = $group.mail
+        Write-Output "[$(get-date -Format G)] Processing Memberships for Group `"$($group.displayName)`" [$($group.Id)] from directory" | out-file $logFilePath -Append
+        Write-Progress -Activity "Processing Memberships for Group..." -Status "Progress ->" -PercentComplete ($index/$groupList.count*100)
+        $groupMembersUri = $graphEndPoint + "/$graphVersion/groups/" + $group.Id + '/members'
         $groupMembers = PageAll-GraphRequest $groupMembersUri $refreshToken $method $graphScopes $logFilePath
-        foreach($groupMember in $groupMembers){
-            $AUObj = New-Object PSObject
-            $AUObj | Add-Member NoteProperty -Name GroupDisplayName -Value $DN
-            $AUObj | Add-Member NoteProperty -Name GroupObjectID -Value $GroupObjectID
-            $AUObj | Add-Member NoteProperty -Name GroupEmailAddress -Value $Addr
-            $AUObj | Add-Member NoteProperty -Name MemberDisplayName -Value $groupMember.displayName
-            $AUObj | Add-Member NoteProperty -Name MemberObjectID -Value $groupMember.id
-            $AUObj | Add-Member NoteProperty -Name MemberPrimarySMTPAddress -Value $groupMember.mail
-            $Results += $AUObj
-        }     
+
+        foreach ($groupMember in $groupMembers) {
+            if ($groupMember.id) {
+                $AUObj = New-Object PSObject
+                $AUObj | Add-Member NoteProperty -Name GroupDisplayName -Value $name
+                $AUObj | Add-Member NoteProperty -Name GroupObjectID -Value $id
+                $AUObj | Add-Member NoteProperty -Name GroupEmailAddress -Value $addr
+                $AUObj | Add-Member NoteProperty -Name MemberDisplayName -Value $groupMember.displayName
+                $AUObj | Add-Member NoteProperty -Name MemberObjectID -Value $groupMember.id
+                $AUObj | Add-Member NoteProperty -Name MemberPrimarySMTPAddress -Value $groupMember.mail
+                $Results += $AUObj
+            }
+        }
+        $index++
     }
     return $Results
 }
@@ -125,13 +131,8 @@ try {
 }
 catch {
     Write-Error "Failed to load Microsoft Graph PowerShell Module."
-    Get-PrerequisiteHelp | Out-String | Write-Error
+    Get-Help -Name .\Export-Class_Membership_for_Restrictions.ps1 -Full | Out-String | Write-Error    
     throw
-}
-
-# Create output folder if it does not exist
-if ((Test-Path $OutFolder) -eq 0) {
-    mkdir $OutFolder;
 }
 
 $refreshToken = Initialize $graphScopes
@@ -140,7 +141,7 @@ $groupList = Get-Groups $graphEndPoint $refreshToken 'GET' $graphScopes $logFile
 
 $groupMembersList = Get-GroupMembers $groupList $graphEndPoint $refreshToken 'GET' $graphScopes $logFilePath
 
-$groupMembersList | Export-Csv "$OutFolder\Export-Class_Membership_for_Restrictions.csv" -NoTypeInformation
+$groupMembersList | Export-Csv "$outFolder\Export-Class_Membership_for_Restrictions.csv" -NoTypeInformation
 
 Write-Output "`nDone.`n"
 
