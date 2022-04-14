daa3c0122d227c88ca237788541ee001d4218450
Tim McCall <timccall@microsoft.com>
2022-04-14 10:18:25 -0400

    Changes made from code review

--- SDS Scripts/Get-All_Students_and_Teachers.ps1
+++ SDS Scripts/Get-All_Students_and_Teachers.ps1
@@ -1,39 +1,52 @@
 ﻿<#
-Script Name:
-Get-All_Students_and_Teachers.ps1
+.Synopsis
+This script is designed to export all students and teachers, into 2 CSV files (Student.csv and Teacher.csv). This script contains a mix of SDS attributes and standard Azure user object attributes. 
 
-Synopsis:
-Description: This script is designed to export all students and teachers, into 2 CSV files (Student.csv and Teacher.csv). This script contains a mix of SDS attributes and standard Azure user object attributes. 
-
-Syntax Examples and Options:
+.Example
 .\Get-All_Students_and_Teachers.ps1
 
-Written By: 
-Orginal/Full Script written by TJ Vering. This script was adapted from the orginal by Bill Sluss.
+.Notes
+========================
+ Required Prerequisites
+========================
+1. Install Microsoft Graph Powershell Module with command 'Install-Module Microsoft.Graph'
+2. Make sure to download common.ps1 to the same folder of the script which has common functions needed.  https://github.com/OfficeDev/O365-EDU-Tools/blob/master/SDS%20Scripts/common.ps1
+3. Check that you can connect to your tenant directory from the PowerShell module to make sure everything is set up correctly.
+    
+    a. Open a separate PowerShell session
+    
+    b. Execute: "connect-graph -scopes User.Read.All, GroupMember.Read.All, Member.Read.Hidden, Group.Read.All, Directory.Read.All, AdministrativeUnit.Read.All" to bring up a sign in UI. 
+    
+    c. Sign in with any tenant administrator credentials
+    
+    d. If you are returned to the PowerShell session without error, you are correctly set up
 
-Change Log:
-Version 1.0, 12/06/2016 - First Draft
-Version 2.0, 03/11/2022 - Change to MS Graph Module - Tim McCall
+4. Retry this script. If you still get an error about failing to load the Microsoft Graph module, troubleshoot why "Import-Module Microsoft.Graph.Authentication -MinimumVersion 0.9.1" isn't working
+
+========================
 
 #>
 
 Param (
-    [string] $ExportStudents = $true,
-    [string] $ExportTeachers = $true,
-    [string] $ExportTeacherRosters = $true,
-    [string] $OutFolder = "./StudentsTeachersExport",
+    [Parameter(Mandatory=$false)]
+    [string] $skipToken = ".",
+    [Parameter(Mandatory=$false)]
+    [string] $outFolder = ".\StudentsTeachersExport",  
+    [Parameter(Mandatory=$false)]
+    [string] $graphVersion = "beta",
+    [Parameter(Mandatory=$false)]
     [switch] $PPE = $false,
-    [switch] $AppendTenantIdToFileName = $false,
-    [Parameter(Mandatory=$false)]
-    [string] $skipToken= ".",
-    [Parameter(Mandatory=$false)]
+    [string] $exportStudents = $true,
+    [string] $exportTeachers = $true,
+    [string] $exportTeacherRosters = $true,
+    [switch] $appendTenantIdToFileName = $false,
     [switch] $downloadCommonFNs = $false
 )
 
-$GraphEndpointProd = "https://graph.windows.net"
-$GraphEndpointPPE = "https://graph.ppe.windows.net"
+$graphEndpointProd = "https://graph.windows.net"
+$graphEndpointPPE = "https://graph.ppe.windows.net"
 
-$logFilePath = $OutFolder
+$logFilePath = "$outFolder\StudentsTeachersExport.log"
 
 $eduObjTeacher = "Teacher"
 $eduObjStudent = "Student"
@@ -43,7 +56,7 @@ if ($downloadCommonFNs){
     # Downloading file with latest common functions
     try {
         Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OfficeDev/O365-EDU-Tools/master/SDS%20Scripts/common.ps1" -OutFile ".\common.ps1" -ErrorAction Stop -Verbose
-        "Grabbed 'common.ps1' to currrent directory"
+        "Grabbed 'common.ps1' to current directory"
     } 
     catch {
         throw "Unable to download common.ps1"
@@ -53,41 +66,10 @@ if ($downloadCommonFNs){
 #import file with common functions
 . .\common.ps1 
 
-function Get-PrerequisiteHelp
-{
-    Write-Output @"
-========================
- Required Prerequisites
-========================
-
-1. Install Microsoft Graph Powershell Module with command 'Install-Module Microsoft.Graph'
-
-2. Make sure to download common.ps1 to the same folder of the script which has common functions needed.  https://github.com/OfficeDev/O365-EDU-Tools/blob/master/SDS%20Scripts/common.ps1
-
-3. Check that you can connect to your tenant directory from the PowerShell module to make sure everything is set up correctly.
-    
-    a. Open a separate PowerShell session
-    
-    b. Execute: "connect-graph -scopes User.Read.All, GroupMember.Read.All, Member.Read.Hidden, Group.Read.All, Directory.Read.All, AdministrativeUnit.Read.All" to bring up a sign in UI. 
-    
-    c. Sign in with any tenant administrator credentials
-    
-    d. If you are returned to the PowerShell sesion without error, you are correctly set up
-
-4. Retry this script.  If you still get an error about failing to load the Microsoft Graph module, troubleshoot why "Import-Module Microsoft.Graph.Authentication -MinimumVersion 0.9.1" isn't working
-
-5. Please visit the following link if a message is received that the license cannot be assigned.
-   https://docs.microsoft.com/en-us/azure/active-directory/enterprise-users/licensing-groups-resolve-problems
-
-(END)
-========================
-"@
-}
-
 function Export-SdsTeachers
 {
-    $fileName = $eduObjTeacher.ToLower() + $(if ($AppendTenantIdToFileName) { "-" + $authToken.TenantId } else { "" }) +".csv"
-	$filePath = Join-Path $OutFolder $fileName
+    $fileName = $eduObjTeacher.ToLower() + $(if ($appendTenantIdToFileName) { "-" + $authToken.TenantId } else { "" }) +".csv"
+	$filePath = Join-Path $outFolder $fileName
     Remove-Item -Path $filePath -Force -ErrorAction Ignore
 
     $data = Get-SdsTeachers
@@ -114,16 +96,16 @@ function Get-SdsTeachers
     
     foreach($user in $users)
     {
-        #DisplayName,UserPrincipalName,SIS ID,School SIS ID,Teacher Number,Status,Secondary Email
+        #DisplayName,UserPrincipalName,SIS ID,School SIS ID,Teacher Number,Status,Secondary Email,ObjectID
         $data += [pscustomobject]@{
-            "DisplayName" = $user.DisplayName  
-            "UserPrincipalName" = $user.userPrincipalName  
+            "DisplayName" = $user.DisplayName
+            "UserPrincipalName" = $user.userPrincipalName
             "SIS ID" = $user.extension_fe2174665583431c953114ff7268b7b3_Education_SyncSource_TeacherId
             "School SIS ID" = $user.extension_fe2174665583431c953114ff7268b7b3_Education_SyncSource_SchoolId
             "Teacher Number" = $user.extension_fe2174665583431c953114ff7268b7b3_Education_TeacherNumber
             "Status" = $user.extension_fe2174665583431c953114ff7268b7b3_Education_TeacherStatus
             "Secondary Email" = $user.extension_fe2174665583431c953114ff7268b7b3_Education_Email
-            "ObjectID" = $user.objectID
+            "ObjectID" = $user.id
         }
     }
     return $data
@@ -136,8 +118,8 @@ function Get-Teachers
 
 function Export-SdsStudents
 {
-    $fileName = $eduObjStudent.ToLower() + $(if ($AppendTenantIdToFileName) { "-" + $authToken.TenantId } else { "" }) +".csv"
-	$filePath = Join-Path $OutFolder $fileName
+    $fileName = $eduObjStudent.ToLower() + $(if ($appendTenantIdToFileName) { "-" + $authToken.TenantId } else { "" }) +".csv"
+	$filePath = Join-Path $outFolder $fileName
     Remove-Item -Path $filePath -Force -ErrorAction Ignore
     
     $data = Get-SdsStudents
@@ -166,13 +148,13 @@ function Get-SdsStudents
         #DisplayName,UserPrincipalName,SIS ID,School SIS ID,Student Number,Status,Secondary Email
         $data += [pscustomobject]@{
             "DisplayName" = $user.displayname
-            "UserPrincipalName" = $user.userPrincipalName 
+            "UserPrincipalName" = $user.userPrincipalName
             "SIS ID" = $user.extension_fe2174665583431c953114ff7268b7b3_Education_SyncSource_StudentId
             "School SIS ID" = $user.extension_fe2174665583431c953114ff7268b7b3_Education_SyncSource_SchoolId
             "Student Number" = $user.extension_fe2174665583431c953114ff7268b7b3_Education_StudentNumber
             "Status" = $user.extension_fe2174665583431c953114ff7268b7b3_Education_StudentStatus
             "Secondary Email" = $user.extension_fe2174665583431c953114ff7268b7b3_Education_Email
-            "ObjectID" = $user.ObjectID
+            "ObjectID" = $user.id
         }
     }
 
@@ -209,11 +191,11 @@ function Get-Users
 }
 
 # Main
-$graphEndPoint = $GraphEndpointProd
+$graphEndPoint = $graphEndpointProd
 
 if ($PPE)
 {
-    $graphEndPoint = $GraphEndpointPPE
+    $graphEndPoint = $graphEndpointPPE
 }
 
 $activityName = "Reading SDS objects in the directory"
@@ -243,15 +225,15 @@ $tenantId =  $tenantInfo.Id
 $tenantDisplayName = $tenantInfo.DisplayName
 $tenantdd =  $tenantDomain.Id
 
-$StudentLicenses = (Get-MgSubscribedSku | Where-Object {$_.SkuPartNumber -match "STANDARDWOFFPACK_IW_STUDENT"}).consumedunits
-$StudentLicensesApplied = ($StudentLicenses | Measure-Object -Sum).sum
-$TeacherLicenses = (Get-MgSubscribedSku | Where-Object {$_.SkuPartNumber -match "STANDARDWOFFPACK_IW_FACULTY"}).consumedunits
-$TeacherLicensesApplied = ($TeacherLicenses | Measure-Object -Sum).sum
+$studentLicenses = (Get-MgSubscribedSku | Where-Object {$_.SkuPartNumber -match "STANDARDWOFFPACK_IW_STUDENT"}).consumedunits
+$studentLicensesApplied = ($studentLicenses | Measure-Object -Sum).sum
+$teacherLicenses = (Get-MgSubscribedSku | Where-Object {$_.SkuPartNumber -match "STANDARDWOFFPACK_IW_FACULTY"}).consumedunits
+$teacherLicensesApplied = ($teacherLicenses | Measure-Object -Sum).sum
 
 # Create output folder if it does not exist
-if ((Test-Path $OutFolder) -eq 0)
+if ((Test-Path $outFolder) -eq 0)
 {
-	mkdir $OutFolder;
+	mkdir $outFolder;
 }
 
 # Export all User of Edu Object Type Teacher/Student
@@ -268,7 +250,7 @@ Write-Host -foregroundcolor green "TenantID is $tenantId"
 Write-Host -foregroundcolor green "Tenant default domain is $tenantdd"
 
 
-Write-Host "The number of student licenses currently applied is $StudentLicensesApplied"
-Write-Host "The number of teacher licenses currently applied is $TeacherLicensesApplied"
+Write-Host "The number of student licenses currently applied is $studentLicensesApplied"
+Write-Host "The number of teacher licenses currently applied is $teacherLicensesApplied"
 
 Write-Output "`n`nDone.  Please run 'Disconnect-Graph' if you are finished.`n"
