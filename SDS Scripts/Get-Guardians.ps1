08ac8fc490abbba549da73722b834156d62a2dd2
Tim McCall <timmccall@Tims-MacBook-Pro.local>
2022-10-21 13:32:14 -0400

    adjusted notes and parameters

--- SDS Scripts/Get-Guardians.ps1
+++ SDS Scripts/Get-Guardians.ps1
@@ -9,23 +9,27 @@
 .Description
     This script will query for a single SDS synced student and export the guardians associated with them to a CSV file.
 .Parameter OutFolder
-    The script will output log file here
+    The script will output log file here. Can be changed to any folder (ex. ".\GuardiansExport")
 .Parameter clientId
     The application Id that has EduRoster.ReadWrite.All permission
 .Parameter clientSecret
     The secret of the application Id that has EduRoster.ReadWrite.All permission
+.Parameter tenantId
+    The Id of the tenant.
+.Parameter certificateThumbprint
+    The certificate thumbprint for the application.
 .Parameter tenantDomain
     The domain name of the tenant (ex. contoso.onmicrosoft.com)
-.Parameter studentAadObjectIds
-    The AAD object Id(s) of the student(s) for whom the guardians needs to be retrieved. 
+.Parameter studentAadObjectId
+    The AAD object Id of the student for whom the guardians needs to be retrieved. 
 .Example
-    Get guardian for one student: .\Get-Guardians.ps1 -OutFolder . -clientId "743f3d66-95aa-41d9-237d-45e961251889" -clientSecret "8bK]-[p19402Ac;Y+7<b>5b" -tenantDomain "contoso.onmicrosoft.com" -studentAadObjectIds "ab043123-00aa-60d9-2ab4-12e961702abc"
+    Get guardian for one student: .\Get-Guardians.ps1 -OutFolder . -clientId "743f3d66-95aa-41d9-237d-45e961251889" -clientSecret "8bK]-[p19402Ac;Y+7<b>5b" -tenantDomain "contoso.onmicrosoft.com" -tenantId 8a572b06-4f46-432f-9185-258f6a8d67e6 -certificateThumbprint <CertificateThumbprint> -studentAadObjectId "ab043123-00aa-60d9-2ab4-12e961702abc"
 .Notes
 ========================
  Required Prerequisites
 ========================
-1. App must be created in customer's Azure account with appropriate app permissions and scopes (Directory.Read.All,EduRoster.Read.All,EduRoster.ReadWrite.All)
-2. App must contain a certificate and clientSecret.
+1. App must be created in customer's Azure account with appropriate app permissions and scopes (EduRoster.Read.All,EduRoster.ReadWrite.All)
+2. App must contain a certificate and clientSecret https://learn.microsoft.com/en-us/azure/active-directory/develop/quickstart-configure-app-access-web-apis
 3. Install Microsoft Graph Powershell Module with command 'Install-Module Microsoft.Graph'
 4. Connect-MgGraph -ClientID 743f3d66-95aa-41d9-237d-45e961251889 -TenantId 8a572b06-4f46-432f-9185-258f6a8d67e6 -CertificateThumbprint <CertificateThumbprint>
 5. Import-Module Microsoft.Graph.Education
@@ -36,19 +40,24 @@
 Param (
     [Parameter(Mandatory = $false)]
     [string] $OutFolder = ".",
-    # [string] $outFolder = ".\GuardiansExport", 
 
     [Parameter(Mandatory = $true)]
     [string] $clientId,
 
     [Parameter(Mandatory = $true)]
+    [string] $tenantId,
+
+    [Parameter(Mandatory = $true)]
     [string] $clientSecret,
+
+    [Parameter(Mandatory = $true)]
+    [string] $certificateThumbprint,
 
     [Parameter(Mandatory = $true)]
     [string] $tenantDomain,
 
-    [Parameter(Mandatory = $false)]
-    [string] $studentAadObjectIds
+    [Parameter(Mandatory = $true)]
+    [string] $studentAadObjectId
 )
 
 function Refresh-AccessToken($authToken, $lastRefreshed) {    
@@ -83,11 +92,13 @@ function Get-AccessToken() {
     return $authToken, $lastRefreshed
 }
 
+Connect-MgGraph -ClientID $clientId -TenantId $tenantId -CertificateThumbprint $certificateThumbprint
+
 function Get-GuardiansForUser($userId, $authToken, $lastRefreshed) {
     $authToken, $lastRefreshed = Refresh-AccessToken -authToken $authToken -lastRefreshed $lastRefreshed
     Write-Progress -Activity "Getting guardians for user $userId"
 
-    $user = invoke-graphrequest -method GET -uri "https://graph.microsoft.com/beta/education/users/$($userid)?`$select=relatedContacts,id,displayName"
+    $user = Invoke-graphrequest -method GET -uri "https://graph.microsoft.com/beta/education/users/$($userid)?`$select=relatedContacts,id,displayName"
 
     $allContacts = $user.relatedContacts
 
@@ -131,9 +142,9 @@ if ($authToken -eq $null) {
     return
 }
 
-if ($studentAadObjectIds -ne "") {
-    $studentAadObjectIdsArr = $studentAadObjectIds.split(',')
-    foreach($studentAadObjectId in $studentAadObjectIdsArr) {
+if ($studentAadObjectId -ne "") {
+    $studentAadObjectIdArr = $studentAadObjectId.split(',')
+    foreach($studentAadObjectId in $studentAadObjectIdArr) {
         Write-Host "Getting guardians for student object Id $studentAadObjectId"
         Get-GuardiansForUser -userId $studentAadObjectId -authToken $authToken -lastRefreshed $lastRefreshed
     }    
